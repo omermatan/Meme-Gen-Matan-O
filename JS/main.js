914baec520041aa21c77c0124fd4c7b21eb36ed1
Matan Omer <mattanomer@gmail.com>
2021-10-28 15:52:12 +0300

    initial

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,61 @@
 'use strict'
-const a = 2;
+
+var gElCanvas;
+var gCtx;
+
+var gImgs = [{id: 1, url: 'img/img01.jpeg', keywords: ['cute', 'funny']},
+{id: 2, url: 'img/img02.jpeg', keywords: ['weird', 'funny']},
+{id: 3, url: 'img/img03.jpeg', keywords: ['weird', 'from movies', 'funny']},
+{id: 4, url: 'img/img04.jpeg', keywords: ['cute', 'sarcastic', 'funny']},
+{id: 5, url: 'img/img05.jpeg', keywords: ['cute', 'sarcastic', 'funny']}]; 
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [{
+        text: 'text',
+        size: 35,
+        align: 'center',
+        color: 'white',
+        stroke: 'black',
+        font: 'Impact',
+        pos: { x: 30, y: 30 },
+        isDrag: false,
+        }]
+    }
+
+
+function init() {
+    gElCanvas = document.querySelector('canvas');
+    gCtx = gElCanvas.getContext('2d');
+    drawImg()
+    drawTextOnCanvas('TRYING', 50, 50);
+
+}
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+} 
+
+function drawTextOnCanvas(text, x, y) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '35px IMPACT';
+    gCtx.fillText(text, x, y) 
+    gCtx.strokeText(text, x, y)
+}
+
+function drawImg() {
+    var img = new Image();
+    img.src = gImgs[1].url;
+    img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    };
+}
+
+function downLoadCan(elLink) {
+    const data = gElCanvas.toDataURL();
+    elLink.href = data;
+    elLink.download = 'meme';
+}
