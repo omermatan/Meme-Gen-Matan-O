f1022b5d053b577c3b8ec5bd4be64cdb567fa723
Matan Omer <mattanomer@gmail.com>
2021-10-29 16:35:17 +0300

    added some stuff

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@
 
 var gElCanvas;
 var gCtx;
+var gCurrImg;
 
 var gImgs = [{id: 1, url: 'img/img01.jpeg', keywords: ['cute', 'funny']},
 {id: 2, url: 'img/img02.jpeg', keywords: ['weird', 'funny']},
@@ -9,33 +10,57 @@ var gImgs = [{id: 1, url: 'img/img01.jpeg', keywords: ['cute', 'funny']},
 {id: 4, url: 'img/img04.jpeg', keywords: ['cute', 'sarcastic', 'funny']},
 {id: 5, url: 'img/img05.jpeg', keywords: ['cute', 'sarcastic', 'funny']}]; 
 
+function getImgs(){
+    return gImgs;
+}
+
+function getImgUrlById(id) {
+    var currImage = gImgs.find(img => img.id === id);
+    gCurrImg = currImage.url;
+    return currImage.url;
+}
+
+function renderGallery() {
+    var imgs = getImgs();
+    var strHtml
+    strHtml = imgs.map(img => {
+        return `<img src="${img.url}" onclick="setCanvasImg('${img.url}')">`
+    })
+    document.querySelector('.gallery-container').innerHTML = strHtml.join('');
+}
+
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
-    lines: [{
-        text: 'text',
-        size: 35,
-        align: 'center',
-        color: 'white',
-        stroke: 'black',
-        font: 'Impact',
-        pos: { x: 30, y: 30 },
-        isDrag: false,
-        }]
-    }
+    // lines: [{
+    //     text: 'text',
+    //     size: 35,
+    //     align: 'center',
+    //     color: 'white',
+    //     stroke: 'black',
+    //     font: 'Impact',
+    //     pos: { x: 30, y: 30 },
+    //     isDrag: false,
+    // }]
+    lines: []
+}
 
 
 function init() {
     gElCanvas = document.querySelector('canvas');
     gCtx = gElCanvas.getContext('2d');
-    drawImg()
     drawTextOnCanvas('TRYING', 50, 50);
+    renderGallery();
 
 }
 
 function clearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
 } 
+function setCanvasImg(imgSrc){
+    drawImg(imgSrc);
+}
+
 
 function drawTextOnCanvas(text, x, y) {
     gCtx.lineWidth = 2;
@@ -46,11 +71,11 @@ function drawTextOnCanvas(text, x, y) {
     gCtx.strokeText(text, x, y)
 }
 
-function drawImg() {
-    var img = new Image();
-    img.src = gImgs[1].url;
+function drawImg(url) {
+    const img = new Image();
+    img.src = url;
     img.onload = () => {
-    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
     };
 }
 
@@ -59,3 +84,10 @@ function downLoadCan(elLink) {
     elLink.href = data;
     elLink.download = 'meme';
 }
+
+function handleTextInput(text){
+    gMeme.lines.push(text);
+    const recentTextIndex = lines.length - 1
+    // push actual object and not simplified styring
+    drawTextOnCanvas(gMeme.lines[recentTextIndex], 50, 50);
+} 
