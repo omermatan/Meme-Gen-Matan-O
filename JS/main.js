59a5f0e62cb5e4c3448b0e9b1b9b18116aed7f32
Matan Omer <mattanomer@gmail.com>
2021-10-30 15:15:58 +0300

    refactor render flow

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,8 @@
 var gElCanvas;
 var gCtx;
 var gCurrImg;
+const gDefaultX = 50;
+const gDefaultY = 50;
 
 var gImgs = [{id: 1, url: 'img/img01.jpeg', keywords: ['cute', 'funny']},
 {id: 2, url: 'img/img02.jpeg', keywords: ['weird', 'funny']},
@@ -30,26 +32,39 @@ function renderGallery() {
 }
 
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgUrl: '',
     selectedLineIdx: 0,
-    // lines: [{
-    //     text: 'text',
-    //     size: 35,
-    //     align: 'center',
-    //     color: 'white',
-    //     stroke: 'black',
-    //     font: 'Impact',
-    //     pos: { x: 30, y: 30 },
-    //     isDrag: false,
-    // }]
-    lines: []
+    lines: [{
+        text: 'Write whatever you think is funny',
+        size: 35,
+        align: 'center',
+        color: 'white',
+        stroke: 'black',
+        font: 'Impact',
+        pos: { x: 30, y: 30 },
+        isDrag: false,
+    }]
+    
+}
+
+function addLine(){
+    gMeme.lines.push({
+        text: 'Write whatever you think is funny',
+        size: 35,
+        align: 'center',
+        color: 'white',
+        stroke: 'black',
+        font: 'Impact',
+        pos: { x: 50, y: 50},
+        isDrag: false,
+    })
 }
 
 
 function init() {
     gElCanvas = document.querySelector('canvas');
     gCtx = gElCanvas.getContext('2d');
-    drawTextOnCanvas('TRYING', 50, 50);
+    drawTextOnCanvas(gMeme.lines[0].text, 50, 50);
     renderGallery();
 
 }
@@ -58,24 +73,34 @@ function clearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
 } 
 function setCanvasImg(imgSrc){
-    drawImg(imgSrc);
+    gMeme.selectedImgUrl = imgSrc;
+    renderCanvas();
+}
+function renderCanvas(){
+    clearCanvas();
+    drawImg();
+    // rendere all line of text
 }
 
 
 function drawTextOnCanvas(text, x, y) {
+    // setCanvasImg(gCurrImg);
+    gCtx.save()
     gCtx.lineWidth = 2;
     gCtx.strokeStyle = 'black';
     gCtx.fillStyle = 'white';
-    gCtx.font = '35px IMPACT';
+    gCtx.font = `${gMeme.lines[0].size}px IMPACT`;
     gCtx.fillText(text, x, y) 
     gCtx.strokeText(text, x, y)
+    gCtx.restore();
 }
 
-function drawImg(url) {
+function drawImg() {
     const img = new Image();
-    img.src = url;
+    img.src = gMeme.selectedImgUrl;
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+
     };
 }
 
@@ -85,9 +110,52 @@ function downLoadCan(elLink) {
     elLink.download = 'meme';
 }
 
-function handleTextInput(text){
-    gMeme.lines.push(text);
-    const recentTextIndex = lines.length - 1
-    // push actual object and not simplified styring
-    drawTextOnCanvas(gMeme.lines[recentTextIndex], 50, 50);
+function handleTextInput(input){
+    const id = +input.id;
+    const text = input.value;
+    const currentLine = gMeme.lines[id];
+    currentLine.text = text;
+    // push actual object and not simplified string
+    drawTextOnCanvas(currentLine.text, 50, 50);
+    console.log(gMeme.lines)
 } 
+
+function textSizeIncrease() {
+    gMeme.lines[0].size *= 1.1;
+    drawTextOnCanvas(gMeme.lines[0].text, gDefaultX, gDefaultY)
+    console.log(gMeme.lines[0].size);
+
+}
+
+function textSizeDecrease() {
+    gMeme.lines[0].size *= 0.9;
+    drawTextOnCanvas(gMeme.lines[0].text, gDefaultX, gDefaultY)
+    console.log(gMeme.lines[0].size);
+
+}
+
+function moveTextUp() {
+    const currentTextPos = gMeme.lines[0].pos.y;
+    var moveBy = 5;
+    drawTextOnCanvas(gMeme.lines[0].text, gMeme.lines[0].pos.x , gMeme.lines[0].pos.y + moveBy);
+}
+
+function moveTextDown() {
+    const currentTextPos = gMeme.lines[0].pos.y;
+    var moveBy = 5;
+    drawTextOnCanvas(gMeme.lines[0].text, gMeme.lines[0].pos.x , gMeme.lines[0].pos.y - moveBy);
+}
+
+function addLine(){
+   var newLine = {
+        text: 'Write whatever you think is funny',
+        size: 35,
+        align: 'center',
+        color: 'white',
+        stroke: 'black',
+        font: 'Impact',
+        pos: { x: 30, y: 30 },
+        isDrag: false,
+    }
+
+}
